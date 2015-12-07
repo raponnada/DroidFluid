176b0adf66e4ec0c2de1ec3ab5945ee8c1bcccc6
Cuong Cu <csc823@gmail.com>
2015-12-06 18:31:10 -0600

    Fix terminated string

--- app/src/main/jni/AndroidSwitch.cc
+++ app/src/main/jni/AndroidSwitch.cc
@@ -17,20 +17,18 @@ void get_leader_ip(char *ip, int len)
     sendto(sockfd, sendBuff, sizeof(sendBuff), 0, (struct sockaddr *)&oracle, sizeof(oracle));
     int recvCount = recvfrom(sockfd, ip, len, 0,  (struct sockaddr *)&oracle, &slen);
 
-    fprintf(stdout, "%s\n", ip);
-    fflush(stdout);
+    ip[recvCount] = '\0';
 }
 
 int main(int argc, char* argv[])
 {
-    char leader_ip[1024];
-
-    get_leader_ip(leader_ip, 1024);
+    char leader_ip[128];
+    get_leader_ip(leader_ip, 128);
 
     std::vector<std::string> ports;
     ports.push_back("wlan0");
 
-    Switch *sw = new Switch(0, &leader_ip, 6653, ports);
+    Switch *sw = new Switch(0, std::string(leader_ip), 6653, ports);
 
     sw->start();
     wait_for_sigint();
